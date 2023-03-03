bddb74edfc844e6bc62d376f59e648d8ab892795
Miko <rosalesmikole@gmail.com>
2023-03-03 11:32:16 +0800

    [Reusable Components] Layout-Container

---
+++ client/src/pages/demo/layouts/container.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from '@/src/shared/layouts/container';
+
+const ContainerDemo: React.FC = () => {
+  return (
+    <Container>
+      <div className="flex justify-center items-center h-screen bg-red-50">
+        Layout Container
+      </div>
+    </Container>
+  );
+};
+
+export default ContainerDemo;
---
+++ client/src/shared/layouts/container/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface ContainerProps {
+  children: any
+}
+
+const Container: React.FC<ContainerProps> = ({ children }: ContainerProps) => {
+  return <div className="container mx-auto px-20">{children}</div>;
+};
+
+export default Container;
