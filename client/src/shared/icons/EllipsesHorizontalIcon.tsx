d5a1e6783f017bef09f7bb32d4428c4df66a4662
epifaniovillaro <122772014+epifaniovillaro@users.noreply.github.com>
2023-03-20 15:56:09 +0800

    [LMS-335][FE]Course Detail Header (#50)
    * [LMS-335][FE]Course Detail Header
    
    * Deleted the two tabs not included
    
    ---------
    
    Co-authored-by: Pif <epifanio.villaro@usjr.edu.ph>

---
+++ client/src/shared/icons/EllipsesHorizontalIcon.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+import type { FC } from 'react';
+
+export interface EllipsesHorizontalIconProps {
+  height?: number;
+  width?: number;
+  classname?: string;
+}
+
+const EllipsesHorizontalIcon: FC<EllipsesHorizontalIconProps> = ({
+  height,
+  width,
+  classname
+}: EllipsesHorizontalIconProps) => {
+  return (
+    <svg
+      height={height}
+      width={width}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      className={classname}
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+      />
+    </svg>
+  );
+};
+
+EllipsesHorizontalIcon.defaultProps = {
+  height: 25,
+  width: 25,
+  classname: ''
+};
+
+export default EllipsesHorizontalIcon;
