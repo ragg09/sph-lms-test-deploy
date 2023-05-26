943047f62c6b93390baefa9f86ba77694a8d70ca
EJ Anton <ejanton.potot@sun-asterisk.com>
2023-05-26 09:44:08 +0800

    LMS-597 [API/ INTEGRATION] Trainer can view a list of courses

--- client/src/pages/trainer/courses/index.tsx
+++ client/src/pages/trainer/courses/index.tsx
@@ -1,150 +1,61 @@
-import React, { useState } from 'react';
-import SearchBar from '@/src/shared/components/SearchBar/SearchBar';
+import { useGetCoursesQuery } from '@/services/courseAPI';
 import Pagination from '@/src/shared/components/Pagination';
-import CourseCard from '../../../shared/components/Card/CourseCard';
+import SearchBar from '@/src/shared/components/SearchBar/SearchBar';
 import type { Course } from '@/src/shared/utils';
+import { alertError } from '@/src/shared/utils';
+import Head from 'next/head';
 import Link from 'next/link';
-
-// SAMPLE COURSE
-export const courses: Course[] = [
-  {
-    id: 1,
-    image: 'https://cdn.idropnews.com/wp-content/uploads/1970/01/23083056/MacBook-Pro.jpg',
-    title: 'Vue Mastery',
-    description: 'Lorem, ipsum dolor sit amet consectetur. Amet senectus augue in urna.',
-    ratings: 2.2,
-    categories: [
-      { id: 1, name: 'JavaScript' },
-      { id: 2, name: 'TypeScript' },
-      { id: 4, name: 'React.js' },
-    ],
-    is_active: false,
-  },
-  {
-    id: 2,
-    image:
-      'https://www.shortform.com/blog/wp-content/uploads/2021/10/smarphone-apple-iphone-laptop-technology-750x350.jpg',
-    title: 'Vue Mastery',
-    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque, aliquam.',
-    ratings: 3.2,
-    categories: [
-      { id: 1, name: 'JavaScript' },
-      { id: 2, name: 'TypeScript' },
-      { id: 4, name: 'React.js' },
-    ],
-    is_active: false,
-  },
-  {
-    id: 3,
-    image:
-      'https://www.simplilearn.com/ice9/free_resources_article_thumb/Top_18_Technical_Courses_After_Graduation.jpg',
-    title: 'Vue Mastery',
-    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque, aliquam.',
-    ratings: 1.3,
-    categories: [
-      { id: 1, name: 'JavaScript' },
-      { id: 2, name: 'TypeScript' },
-      { id: 4, name: 'React.js' },
-    ],
-    is_active: false,
-  },
-  {
-    id: 4,
-    image:
-      'https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
-    title: 'Vue Mastery',
-    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque, aliquam.',
-    ratings: 3,
-    categories: [
-      { id: 1, name: 'JavaScript' },
-      { id: 2, name: 'TypeScript' },
-      { id: 4, name: 'React.js' },
-    ],
-    is_active: false,
-  },
-  {
-    id: 5,
-    image:
-      'https://campustechnology.com/-/media/EDU/CampusTechnology/2019-Images/20190710onlinelearning.jpg',
-    title: 'Vue Mastery',
-    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque, aliquam.',
-    ratings: 5.0,
-    categories: [
-      { id: 1, name: 'JavaScript' },
-      { id: 2, name: 'TypeScript' },
-      { id: 4, name: 'React.js' },
-    ],
-    is_active: false,
-  },
-  {
-    id: 6,
-    image:
-      'https://res.cloudinary.com/highereducation/images/f_auto,q_auto/v1664380340/AccreditedSchoolsOnline.org/GettyImages-1221204650_8194983b6/GettyImages-1221204650_8194983b6.jpg ',
-    title: 'Vue Mastery',
-    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque, aliquam.',
-    ratings: 3.4,
-    categories: [
-      { id: 1, name: 'JavaScript' },
-      { id: 2, name: 'TypeScript' },
-      { id: 4, name: 'React.js' },
-    ],
-    is_active: false,
-  },
-  {
-    id: 7,
-    image: 'https://cdn.idropnews.com/wp-content/uploads/1970/01/23083056/MacBook-Pro.jpg',
-    title: 'Vue Mastery',
-    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque, aliquam.',
-    ratings: 2.2,
-    categories: [
-      { id: 1, name: 'JavaScript' },
-      { id: 2, name: 'TypeScript' },
-      { id: 4, name: 'React.js' },
-    ],
-    is_active: false,
-  },
-  {
-    id: 8,
-    image:
-      'https://www.shortform.com/blog/wp-content/uploads/2021/10/smarphone-apple-iphone-laptop-technology-750x350.jpg',
-    title: 'Vue Mastery',
-    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque, aliquam.',
-    ratings: 3.2,
-    categories: [
-      { id: 1, name: 'JavaScript' },
-      { id: 2, name: 'TypeScript' },
-      { id: 4, name: 'React.js' },
-    ],
-    is_active: false,
-  },
-  {
-    id: 9,
-    image:
-      'https://www.simplilearn.com/ice9/free_resources_article_thumb/Top_18_Technical_Courses_After_Graduation.jpg',
-    title: 'Vue Mastery',
-    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque, aliquam.',
-    ratings: 1.3,
-    categories: [
-      { id: 1, name: 'JavaScript' },
-      { id: 2, name: 'TypeScript' },
-      { id: 4, name: 'React.js' },
-    ],
-    is_active: false,
-  },
-];
-
-const handleSearch = (query: string): void => {
-  alert(`You Searched "${query}"`);
-};
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import CourseCard from '../../../shared/components/Card/CourseCard';
 
 const CoursesListPage: React.FC = () => {
-  const [currentPage, setCurrentPage] = useState(1);
+  const router = useRouter();
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState('');
+
+  const {
+    data: { results: courses = [], totalPages, current_page_number: currentPage } = {},
+    isLoading,
+    error,
+    refetch,
+  } = useGetCoursesQuery({ search, page });
+
+  useEffect(() => {
+    if (search === '') {
+      const { query } = router;
+      delete query.search;
+      void router.replace({ query });
+    }
+  }, [search, refetch]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return alertError('An error occured');
+  }
+
+  const handleSearch = (search: string): void => {
+    if (search) {
+      void router.push({
+        pathname: router.pathname,
+        query: { ...router.query, search },
+      });
+    }
+    setSearch(search);
+  };
 
   const handleChangePageEvent = (page: number): void => {
-    setCurrentPage(page);
+    setPage(page);
   };
+
   return (
     <>
+      <Head>
+        <title>Courses</title>
+      </Head>
       <div>
         <div className="flex justify-center">
           <div className="container mt-4">
@@ -162,18 +73,25 @@ const CoursesListPage: React.FC = () => {
               </Link>
             </div>
             <div className="flex justify-center mb-4">
-              <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-                {courses.map((course) => (
-                  <CourseCard key={course.id} course={course} />
-                ))}
-              </div>
+              {/* eslint-disable-next-line multiline-ternary */}
+              {!courses.length ? (
+                <div className="flex items-center text-sm text-gray-400 h-24">
+                  No courses to show
+                </div>
+              ) : (
+                <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+                  {courses.map((course: Course) => (
+                    <CourseCard key={course.id} course={course} />
+                  ))}
+                </div>
+              )}
             </div>
           </div>
         </div>
         <div className="flex justify-center items-center my-5">
           <Pagination
             maxPages={5}
-            totalPages={10}
+            totalPages={totalPages}
             currentPage={currentPage}
             onChangePage={handleChangePageEvent}
           />
--- client/src/shared/components/Card/CourseCard/index.tsx
+++ client/src/shared/components/Card/CourseCard/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
 import type { Course } from '@/src/shared/utils';
 import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
 interface Props {
   course: Course;
@@ -12,42 +13,44 @@ const generateStarRating = (rating: number): string => {
   return `${'★'.repeat(roundedRating)}${'☆'.repeat(starCount - roundedRating)}`;
 };
 
-const CourseCard: React.FC<Props> = ({ course }: Props): JSX.Element => {
+const CourseCard: React.FC<Props> = ({ course }: Props) => {
   return (
     <div className="w-[268px] rounded-lg group border border-gray-300 shadow-[2.0px_2.0px_7.0px_rgba(0,0,0,0.1)]">
-      <div className="relative overflow-hidden">
-        <Image
-          className="w-full h-[128px] rounded-tl-lg rounded-tr-lg object-cover transition-transform duration-300 group-hover:scale-110 cursor-pointer"
-          src={course.image}
-          alt={course.title}
-          width={720}
-          height={128}
-        />
-      </div>
-      <div className="p-3">
-        <div className="text-lg font-semibold">{course.title}</div>
-        <div className="text-xs text-gray-400">{course.description}</div>
-        <div className="flex items-center gap-1 mb-3">
-          <span className="text-lightBrown text-sm font-semibold">
-            {course.ratings}
-          </span>
-          <span className="flex text-[#ffd700] text-lg">
-            {generateStarRating(course.ratings)}
-          </span>
+      <Link href={`/trainer/courses/${course.id}`}>
+        <div className="relative overflow-hidden">
+          <Image
+            className="w-full h-[128px] rounded-tl-lg rounded-tr-lg object-cover transition-transform duration-300 group-hover:scale-110 cursor-pointer"
+            src={course.img_path ?? '/image1.jpg'}
+            alt={course.name}
+            width={720}
+            height={128}
+          />
         </div>
-
-        <div className="text-sm font-semibold mb-1">Categories:</div>
-        <div className="relative flex justify-between">
-          {course.categories.map((category) => (
-            <span
-              className="text-xs font-semibold text-gray-400 border-2 border-gray-400 px-2 py-1 my-1 rounded-full transition-color duration-300 hover:bg-gray-50"
-              key={category.id}
-            >
-              {category.name}
+        <div className="p-3">
+          <div className="text-lg font-semibold">{course.name}</div>
+          <div className="text-xs text-gray-400">
+            {course.description ?? 'No description to show'}
+          </div>
+          <div className="flex items-center gap-1 mb-3">
+            <span className="text-lightBrown text-sm font-semibold">{course.ratings}</span>
+            <span className="flex text-[#ffd700] text-lg">
+              {generateStarRating(course.ratings)}
             </span>
-          ))}
+          </div>
+
+          <div className="text-sm font-semibold mb-1">Categories:</div>
+          <div className="relative flex justify-between">
+            {course.categories.map((category, index) => (
+              <span
+                className="text-xs font-semibold text-gray-400 border-2 border-gray-400 px-2 py-1 my-1 rounded-full transition-color duration-300 hover:bg-gray-50"
+                key={index}
+              >
+                {category.name}
+              </span>
+            ))}
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
